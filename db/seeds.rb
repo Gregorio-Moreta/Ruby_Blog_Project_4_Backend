646de5bb22cf48b036b8f6e197daeebc4f84a398
Gregorio Moreta <gregoriomoreta4@gmail.com>
2022-01-04 03:26:02 -0500

    reseeding database with a test user and test post to check routes and json web token availability in postman

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# user = User.create(username: "test", password: "test")
+user = User.create(username: "test", password: "test")
 
-# post = Post.create(user_id: "1", title: "test", author: "test", body: "test")
+post = Post.create(user_id: "1", title: "test", author: "test", body: "test")

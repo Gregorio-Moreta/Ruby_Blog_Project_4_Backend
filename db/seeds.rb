58221650fa1156ac12500de3f65779e896c43bb8
Gregorio Moreta <gregoriomoreta4@gmail.com>
2022-01-03 19:08:53 -0500

    updated JWT authentication routes protecting index and create routes, tested all routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# user = User.create(username: "Greg", password: "Cheeseburger")
+user = User.create(username: "test", password: "test")
 
-post = Post.create(user_id: "3", title: "Cheeseburger", author: "Greg -Writer", body: "lorem ipsum and lets see if seeding this works or i might have to create a database")
+post = Post.create(user_id: "4", title: "Cheeseburger", author: "Greg -Writer", body: "lorem ipsum and lets see if seeding this works or i might have to create a database")
